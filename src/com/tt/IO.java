cf681c40799afca2127535c8ff793a4993554d28
pinkeypatel <charu.patel2015@gmail.com>
2022-01-03 12:18:41 -0600

    commit mess

---
+++ src/com/tt/IO.java
@@ -0,0 +1,24 @@
+/*
+ * package com.tt;
+ * 
+ * import java.io.File; import java.io.FileInputStream; import
+ * java.io.FileNotFoundException; import java.io.IOException;
+ * 
+ * public class IO {
+ * 
+ * public static void main(String[] args) { String path1 =
+ * "C:\\Users\\suran\\OneDrive\\Desktop\\note.txt"; String path2 =
+ * "C:\\Users\\suran\\OneDrive\\Desktop\\dest.txt";
+ * 
+ * 
+ * File f1 = new File(path1); File f2 = new File(path2);
+ * 
+ * FileInputStream fr = null; FileInputStream fw = null; fr = new
+ * FileInputStream(f1); fw = new FileInputStream(f2);
+ * 
+ * int i ; while((i = fr.read()) != -1) {
+ * 
+ * ((Object) fw).write(i); } }
+ * 
+ * }
+ */
